cc91f41c4cbcf425a1a29fd2aea79d2379bc537e
dertaya <2654354140@qq.com>
2023-01-09 17:50:24 +0800

    初始化项目

---
+++ app/src/main/java/com/example/gong_native/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gong_native.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gong_native.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
